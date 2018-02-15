da8dddcb6d7e6749a7a22edd66f0492131316e8e
Valentin Pinkau <valentin.pinkau@googlemail.com>
2018-02-15 09:47:01 -0800

    add dummy wrapper

---
+++ src/python/starspace_wrapper.cpp
@@ -0,0 +1,39 @@
+//
+// Created by Valentin Pinkau on 14.02.18.
+//
+
+#include <pybind11/pybind11.h>
+
+int add(int i, int j) {
+    return i + j;
+}
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(starspace, m) {
+m.doc() = R"pbdoc(
+        Pybind11 example plugin
+        -----------------------
+        .. currentmodule:: cmake_example
+        .. autosummary::
+           :toctree: _generate
+           add
+           subtract
+    )pbdoc";
+
+m.def("add", &add, R"pbdoc(
+        Add two numbers
+        Some other explanation about the add function.
+    )pbdoc");
+
+m.def("subtract", [](int i, int j) { return i - j; }, R"pbdoc(
+        Subtract two numbers
+        Some other explanation about the subtract function.
+    )pbdoc");
+
+#ifdef VERSION_INFO
+m.attr("__version__") = VERSION_INFO;
+#else
+m.attr("__version__") = "dev";
+#endif
+}
